08ee5cc141c62f11a2bd2054e060c274f08661a7
Augusto <augus_borromeo@hotmail.com>
2022-05-10 01:28:12 -0300

    desarrollando el inicio de la página

---
+++ menu.js
@@ -0,0 +1,16 @@
+const menuIcon = document.querySelector('#menuIcon')
+menu = document.querySelector('#menu-content')
+
+menuIcon.addEventListener('click', (e) => {
+
+    menu.classList.toggle('activate');
+    document.body.classList.toggle('opacity')
+
+    const route = e.target.getAttribute('src');
+
+    if(route == "./main/imgs/burger-menu.svg") {
+        e.target.setAttribute('src', "./main/imgs/burger-menu-clear.svg")
+    } else {
+        e.target.setAttribute('src', "./main/imgs/burger-menu.svg")
+    }
+})
