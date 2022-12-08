c1f8b90630a659b64ed5e8fdcee27a82c1386ea3
Augusto <64516928+augustobor@users.noreply.github.com>
2022-12-08 16:36:31 -0300

    Update muestra.php

--- menteslibres/muestra.php
+++ menteslibres/muestra.php
@@ -11,7 +11,7 @@
     ?>
 
     <link rel="shortcut icon" href="../assets/favicon.ico">
-
+    <meta name="description" content="Mentes Libres Obra: Augusto.B. Mentes Libres">
     <link rel="stylesheet" type="text/css" href="./obras/controller/styles/style.css">
     <link rel="stylesheet" type="text/css" href="./obras/controller/styles/tablet.css" media="screen and (min-width: 680px)">
     <link rel="stylesheet" type="text/css" href="./obras/controller/styles/desktop.css" media="screen and (min-width: 800px)"> 
