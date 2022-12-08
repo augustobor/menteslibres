003195340fce0ed2015eaf32da2cc41afb1528cd
Augusto <64516928+augustobor@users.noreply.github.com>
2022-12-08 16:33:36 -0300

    Update inicio.php

--- menteslibres/inicio.php
+++ menteslibres/inicio.php
@@ -7,7 +7,8 @@
     ?>
 
     <link rel="shortcut icon" href="../assets/favicon.ico">
-
+    <meta name="description" content="Mentes Libres Home Page: Augusto.B. Mentes Libres">
+    
     <link rel="stylesheet" type="text/css" href="./main/styles/style.css">
     <link rel="stylesheet" type="text/css" href="./main/styles/tablet.css" media="screen and (min-width: 680px)">
     <link rel="stylesheet" type="text/css" href="./main/styles/desktop.css" media="screen and (min-width: 800px)">    
