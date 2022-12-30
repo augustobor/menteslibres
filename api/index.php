b99d971b5eaa66e29feab195b1aebac075064e62
Augusto <64516928+augustobor@users.noreply.github.com>
2022-12-30 17:22:34 -0300

    Delete now.json

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
     header('content-type: application/json');
+<<<<<<< HEAD
     header("Location: ./index.html");
+=======
+    header("Location: ./inicio.html");
+>>>>>>> 9de1c8e (Delete now.json)
 ?>
