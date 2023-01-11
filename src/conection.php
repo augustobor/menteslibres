7202c6f1e192a50e934c6aec9f19645e09e93a6d
augustobor <augus_borromeo@hotmail.com>
2023-01-11 20:24:12 -0300

    adding reject option to connect

--- src/conection.php
+++ src/conection.php
@@ -1,10 +1,10 @@
 <?php
+    error_reporting(0);
     try {
         $string="host=$_ENV[PG_HOST] dbname=$_ENV[PG_DB_NAME] user=$_ENV[PG_USER] password=$_ENV[PG_PASS]";
-        $conexion = pg_connect($string) or die('Could not connect: '. pg_last_error());
+        $conexion = pg_connect($string) or die('<h4>Lo sentimos. Tenemos un error en el servidor :(</h4>'. pg_last_error());
     } catch (PDOEception $e) {
         echo "La conexión falló: . $e->getMessage()";
-
     }
 
     return $conexion;
